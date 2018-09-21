40c9122968ba3ff033bae51c6066a923cd88d6dc
Estudiantes <Estudiantes@Labing50405.udistrital.edu.co>
2018-09-21 11:48:58 -0500

    Versión 1.8

--- src/MiAhorcado/Clase1.java
+++ src/MiAhorcado/Clase1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MiAhorcado;
 
 import java.util.Random;
@@ -14,11 +10,11 @@ public class Clase1 {
 String [] palabra = {"casa","gato","loro"};    
  
  void seleccionarPalabra(){
-    lapalabrita mi1 = new lapalabrita();
+    String mipalabrita;
     Random miRandom = new Random();
     
     int palabraseleccionada = miRandom.nextInt(2);
-    mi1.laq = palabra[palabraseleccionada]; 
+    mipalabrita = palabra[palabraseleccionada]; 
    
 }
  
